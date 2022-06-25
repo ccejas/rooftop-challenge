feab968c618b7da829aeaa09b377a5f4437f1422
cristian cejas <cristian.cejas@gm2dev.com>
2022-06-25 11:12:48 -0300

    feat(getBlock): add getBlocks method and print result

--- index.js
+++ index.js
@@ -14,9 +14,22 @@ async function getToken() {
   return body.token;
 }
 
+function getBlocksEndpoint(token) {
+  return `${process.env.BASE_URL}/blocks?token=${token}`;
+}
+
+async function getBlocks(token) {
+  const response = await fetch(getBlocksEndpoint(token));
+  const body = await response.json();
+  return body.data;
+}
+
 async function main() {
   const token = await getToken();
   console.log("TOKEN:", token);
+
+  const blocks = await getBlocks(token);
+  console.log("BLOCKS:", blocks);
 }
 
 main()
