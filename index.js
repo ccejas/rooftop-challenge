e91c5d86e8d3fa7252fca13cc3412700fe431b13
cristian cejas <cristian.cejas@gm2dev.com>
2022-06-25 11:14:56 -0300

    feat(check): add check method to sort blocks and print result

--- index.js
+++ index.js
@@ -24,12 +24,57 @@ async function getBlocks(token) {
   return body.data;
 }
 
+function getCheckBlocksEndpoint(token) {
+  return `${process.env.BASE_URL}/check?token=${token}`;
+}
+
+async function areConsecutiveBlocks(block1, block2, token) {
+  const response = await fetch(getCheckBlocksEndpoint(token), {
+    method: "POST",
+    body: JSON.stringify({
+      blocks: [block1, block2],
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+  const body = await response.json();
+  return body.message;
+}
+
+async function check(blocks, token) {
+  console.group();
+  const result = [blocks[0]];
+  do {
+    const orderedTokensLength = result.length;
+    console.info("searching next ordered block...");
+    for (let index = 1; index < blocks.length; index++) {
+      if (
+        await areConsecutiveBlocks(
+          result[orderedTokensLength - 1],
+          blocks[index],
+          token
+        )
+      ) {
+        result.push(blocks[index]);
+        console.info("found it!");
+        break;
+      }
+    }
+    if (orderedTokensLength === result.length) throw `Blocks can't be ordered!`;
+  } while (result.length !== blocks.length);
+  console.groupEnd();
+  return result;
+}
+
 async function main() {
   const token = await getToken();
   console.log("TOKEN:", token);
 
   const blocks = await getBlocks(token);
   console.log("BLOCKS:", blocks);
+
+  const sortedBlocks = await check(blocks, token);
+  console.log("SORTED BLOCKS:");
+  console.table(sortedBlocks);
 }
 
 main()
